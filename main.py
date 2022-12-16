4052c68397db8b6fd116f6363169b544537e09b7
Дмитрий Федотов <93176555+Fedott16319@users.noreply.github.com>
2022-12-16 22:20:51 +0400

    дз ко 2 уроку

---
+++ main.py
@@ -0,0 +1,13 @@
+# Напишите программу, которая принимает на вход вещественное число и показывает сумму его цифр.
+# Пример:
+# - 6,782 -> 23
+# - 0,56 -> 11
+
+def summa (number):
+    summa = 0
+    for i in number:
+        if i != '.':
+            summa = summa + int(i)
+    return summa
+number = input('Введите вещественное число: ')
+print (sum(number))
